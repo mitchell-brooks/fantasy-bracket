a3ca3801d09c4dce7a88b184607b7f6f3c7fb0d4
Mitchell Brooks <MitchellBrooks@gmail.com>
2024-03-25 01:09:11 -0400

    Feat: Add strikethrough for eliminated players

--- src/app/pool/[pool_id]/roster/[roster_id]/page.tsx
+++ src/app/pool/[pool_id]/roster/[roster_id]/page.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import { createClient } from '@utils/supabase-server';
-import { Table } from '@components/table/table';
-import { GridTitle } from '@components/grid-title/grid-title';
-import Link from 'next/link';
+import React from "react";
+import { createClient } from "@utils/supabase-server";
+import { Table } from "@components/table/table";
+import { GridTitle } from "@components/grid-title/grid-title";
+import Link from "next/link";
+import { Column } from "react-table";
 
 export default async function PoolIdDraftResultsDraftNumUsernamePage({
-  params: { pool_id, roster_id },
-}: {
+                                                                       params: { pool_id, roster_id }
+                                                                     }: {
   params: { pool_id: string; roster_id: string | number };
 }) {
 
   const supabase = createClient();
   const { data: roster_data_results, error } = await supabase
-    .from('roster_player_total_scores_view')
+    .from("roster_player_total_scores_view")
     .select(
-      'player_name, team_name, seed, total_player_points, pick_number, team_unique, username'
+      "player_name, team_name, seed, total_player_points, pick_number, team_unique, username"
     )
-    .eq('pool_id', pool_id)
-    .eq('roster_id', roster_id);
+    .eq("pool_id", pool_id)
+    .eq("roster_id", roster_id);
 
   const username = roster_data_results?.[0]?.username as string;
 
@@ -32,33 +33,43 @@ export default async function PoolIdDraftResultsDraftNumUsernamePage({
         <Link href={`/pool/${pool_id}/team/${player.team_unique}`}>
           {player.team_name}
         </Link>
-      ),
+      )
     };
   });
 
-  const columns = [
-    {
-      Header: 'Player',
-      columns: [
-        { Header: 'Name', accessor: 'player_name' },
-        { Header: 'Points', accessor: 'total_player_points' },
-      ],
-    },
-    {
-      Header: 'Team',
-      columns: [
-        { Header: 'Team', accessor: 'team_name' },
-        { Header: 'Seed', accessor: 'seed' },
-      ],
-    },
-    {
-      Header: 'Pick',
-      columns: [{ Header: 'Pick', accessor: 'pick_number' }],
-    },
-  ];
+  const columns: Column<Record<string, any>>[] = [
+      {
+        Header: "Player",
+        columns: [
+          {
+            Header: "Name",
+            accessor: (row) =>
+              row?.eliminated ? <s>{row.player_name}</s> : row.player_name
+          },
+          { Header: "Points", accessor: "total_player_points" }
+        ]
+      }
+      ,
+      {
+        Header: "Team",
+        columns:
+          [
+            { Header: "Team", accessor: "team_name" },
+            { Header: "Seed", accessor: "seed" }
+          ]
+      }
+      ,
+      {
+        Header: "Pick",
+        columns:
+          [{ Header: "Pick", accessor: "pick_number" }]
+      }
+
+    ]
+  ;
   return (
     <>
-      <GridTitle title={username ? `${username.toUpperCase()}` : 'Roster'} />
+      <GridTitle title={username ? `${username.toUpperCase()}` : "Roster"} />
       <Table columns={columns} data={mappedRosterData || []} />
     </>
   );
